6534177668b9e1993ea8499fda8e150f05a366ac
Gaurav Pathak <119441625+Gauravpathak1789@users.noreply.github.com>
2023-02-26 11:53:10 +0530

    Add files via upload

---
+++ FLASK--2-.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q1. Explain GET and POST methods
+
+# In[1]:
+
+
+#The GET method is used to retrieve data from a server. When a GET request is made, the server returns the requested resource, which is usually a webpage or data in some other format such as JSON or XML.
+#The POST method is used to submit data to a server. When a POST request is made, the data is sent in the body of the request, rather than being appended to the URL like in a GET request.
+
+
+# Q2. Why is request used in Flask?
+
+# In[2]:
+
+
+#In Flask, request is a built-in object that represents an incoming client request. It is used to access the data submitted by the client in the request such as form data, query parameters, and request headers.
+
+
+# 3. Why is redirect() used in Flask?
+
+# In[3]:
+
+
+#In Flask, redirect() is a built-in function that is used to redirect a client to a different URL. This function is often used in web applications to redirect the user to a different page after an action has been taken or to handle error conditions.
+
+
+# Q4. What are templates in Flask? Why is the render_template() function used?
+
+# In[4]:
+
+
+#In Flask, a template is a file that contains HTML and other markup, as well as placeholders for dynamic content. Templates are used to generate HTML pages that can be sent to the client in response to a request.
+# because---This function takes the name of a template file as its first argument, and any additional arguments are used to pass data to the template. The function returns a string containing the rendered template, which can be sent to the client in the response.
+
+
+# In[ ]:
+
+
+
+
