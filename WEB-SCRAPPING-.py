933931ea9367e313f014c253b2d661ba27945c6c
Gaurav Pathak <119441625+Gauravpathak1789@users.noreply.github.com>
2023-02-26 11:59:36 +0530

    Add files via upload

---
+++ WEB-SCRAPPING-.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q1. What is Web Scraping? Why is it Used? Give three areas where Web Scraping is used to get data.
+
+# In[1]:
+
+
+#Web scraping refers to the automated process of extracting data from websites. It involves using a software tool or a script to crawl through web pages, locate relevant data, and extract it in a structured format.
+#Web scraping is commonly used to gather large volumes of data from various websites. This data can be analyzed to identify patterns and trends, or it can be used to build predictive models.
+
+#E-commerce: Web scraping can be used to collect product information, pricing, and availability data from e-commerce websites.
+
+#Social media: Web scraping can be used to gather data from social media platforms, such as Twitter and Facebook, to monitor trends, sentiment, and user behavior.
+
+#Research: Web scraping can be used to gather data for research purposes, such as collecting data on public opinion, c
+#
+#
+
+
+# Q2. What are the different methods used for Web Scraping?
+
+# In[2]:
+
+
+#Manual scraping: This involves manually copying and pasting data from websites into a spreadsheet or a database. It is a time-consuming process and is usually only feasible for small amounts of data.
+
+#Parsing HTML: This involves parsing the HTML code of a website to extract the relevant data. This can be done using programming languages such as Python, Ruby, or JavaScript. Libraries such as BeautifulSoup, lxml, and HTMLParser can be used to parse the HTML.
+
+#Web scraping tools: There are many web scraping tools available that can automate the scraping process. These tools typically have a user-friendly interface that allows users to select the data they want to extract and specify the websites they want to scrape.
+
+
+# Q3. What is Beautiful Soup? Why is it used?
+
+# In[3]:
+
+
+#Beautiful Soup is a Python library used for web scraping purposes. It allows developers to parse HTML and XML documents and extract useful information from them.
+#The library is widely used in the field of data mining, web scraping, and information retrieval to extract information from websites. Beautiful Soup can handle malformed or invalid markup and can parse documents from various sources like local files, URLs, or even in-memory strings
+
+
+# Q4. Why is flask used in this Web Scraping project?
+
+# In[4]:
+
+
+#flask is suitable for creating api  which helps in craeting server and function through  whhich we can store the dat extracted from scrapping into database and retrieve it when users want
+
